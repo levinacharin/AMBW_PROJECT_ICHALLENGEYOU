af10986e7ff6de4553793889c89f47588fc2688c
levinacharin <75989290+levinacharin@users.noreply.github.com>
2022-06-09 14:33:07 +0700

    HEHE

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,91 @@ class _MyAppState extends State<MyApp> {
           title: Text("I Challenge You"),
         ),
         body: Container(
-          
+          child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(133, 50, 133, 50),
+                    decoration: new BoxDecoration(
+                      color: Colors.orange[50]
+                    ),
+                    child: Text("I Challenge You",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Text("BMI CALCULATOR")),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                            Icon(Icons.person),
+                            Icon(Icons.person)
+                          ],),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Column(children: [
+                          Text("Input Your Age:"),
+                          TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[350],
+                              border: OutlineInputBorder(),
+                            ),
+                          )
+                          ],),)
+                        ,
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Column(children: [
+                          Text("Input Your Height (cm):"),
+                          TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[350],
+                              border: OutlineInputBorder(),
+                            ),
+                          )
+                          ],),)
+                        ,
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Column(children: [
+                          Text("Input Your Weight (kg):"),
+                          TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[350],
+                              border: OutlineInputBorder(),
+                            ),
+                          )
+                          ],),)
+                        ,
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){}, child: Text("SUBMIT"))
+                ],
+              )
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
                 icon: new Icon(Icons.calculate, color: Colors.blue,),
-                //title: new Text("BMI",)
+                title: new Text("BMI",)
             ),
             BottomNavigationBarItem(
                 icon: new Icon(Icons.assignment),
-                //title: new Text("Challenges")
+                title: new Text("Challenges")
             ),
             BottomNavigationBarItem(
                 icon: new Icon(Icons.account_circle),
-                //title: new Text("Profile")
+                title: new Text("Profile")
             ),
             
                 

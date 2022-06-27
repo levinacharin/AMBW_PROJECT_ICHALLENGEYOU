461fe16ea13ad8734328f0a1cc08980d028699e2
levinacharin <75989290+levinacharin@users.noreply.github.com>
2022-06-27 21:48:38 +0700

    detail

--- lib/challenges.dart
+++ lib/challenges.dart
@@ -14,13 +14,22 @@ class challenges extends StatefulWidget {
 class _challengesState extends State<challenges> {
   int _jumlah = 0;
 
-  Stream<QuerySnapshot<Object?>> onSearch(){
+  Stream<QuerySnapshot<Object?>> listchallengeuser(){
     setState(() {
       
     });
     return Database.getlistchallenge('levinacharin7@gmail.com');
   }
-  //Stream<QuerySnapshot<Object?>> txtsearch = Database.getlistchallenge('levinacharin7@gmail.com');
+  
+
+  // Stream<QuerySnapshot<Object?>> detaillistchallenge(){
+  //   setState(() {
+      
+  //   });
+  //   return Database.getdetailchallenge('levinacharin7@gmail.com');
+  // }
+  // Stream<QuerySnapshot<Object?>> txtsearch = Database.getlistchallenge('levinacharin7@gmail.com');
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +41,16 @@ class _challengesState extends State<challenges> {
         //   title: Text("I Challenge You"),
         // ),
         body: Container(
+          decoration: new BoxDecoration(
+                      color: Colors.orange[50]
+                    ),
           child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.fromLTRB(133, 50, 133, 50),
-                    decoration: new BoxDecoration(
-                      color: Colors.orange[50]
-                    ),
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 50),
+                    // decoration: new BoxDecoration(
+                    //   color: Colors.orange[50]
+                    // ),
                     child: Text("I Challenge You",
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
                   ),
@@ -73,9 +85,10 @@ class _challengesState extends State<challenges> {
                       ],
                     ),
                   ),
+                  SizedBox(height: 20,),
                   Expanded(
                           child: StreamBuilder<QuerySnapshot>(
-                            stream: onSearch(),
+                            stream: listchallengeuser(),
                             builder: (context,snapshot){
                               if(snapshot.hasError){
                                 return Text('ERROR');
@@ -84,16 +97,43 @@ class _challengesState extends State<challenges> {
                                   itemBuilder: (context, index){
                                     DocumentSnapshot dsData = snapshot.data!.docs[index];
                                     //mengambil data per index ke dalam variabel data
+                                    String c_descchallenge = dsData['challengedesc'];
                                     String c_status = dsData['status'];
-                                    //String lvIsi = dsData['isiCat'];
+                                    String c_img = dsData['imgchallenge'].toString();
                                     _jumlah = snapshot.data!.docs.length;
-                                    return ListTile(
+                                    var coloricon = Colors.red;
+                                    int stat = 0xf616;
+                                    if(c_status == "done"){
+                                      stat = 0xf635;
+                                      coloricon = Colors.green;
+                                    }
+                                    
+                                    return Card(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(40),
+                                        side: BorderSide(color: Colors.grey.withOpacity(0.2),
+                                        width: 1)
+                                      ),
+                                      elevation: 5,
                                       
-                                      title: Text(c_status),
-                                      //subtitle: Text(lvIsi),
+                                      child: ListTile(
+                                        // shape: RoundedRectangleBorder(
+                                        //   borderRadius: BorderRadius.circular(15),
+                                        //   elevation
+                                        //   side: BorderSide(color: Colors.black)
+                                        // ),
+                                        leading: CircleAvatar(
+                                          backgroundImage:NetworkImage("assets/images/\$c_img.jpg"),
+                                        ),
+                                        title: Text(c_descchallenge),
+                                        //subtitle: Text(c_status),
+                                        trailing: Icon(IconData(stat, fontFamily: 'MaterialIcons'),
+                                        color: coloricon)
+                                        
+                                      ),
                                     );
                                   }, 
-                                  separatorBuilder: (context, index) => const SizedBox(height: 20.0,), 
+                                  separatorBuilder: (context, index) => const SizedBox(height: 10.0,), 
                                   itemCount: snapshot.data!.docs.length
                                   );
                               }
@@ -107,6 +147,7 @@ class _challengesState extends State<challenges> {
                             },
                           ),
                         ),
+
                   
                 ],
               )
